ad5e7ae7dacc5cc2253a0e45bea1d75232f83def
Radim Mifka <radimmifka@gmail.com>
2024-05-04 01:14:17 +0200

    docs: ControlsWidget doxygen

--- include/GUI/Widget/ControlsWidget.hpp
+++ include/GUI/Widget/ControlsWidget.hpp
@@ -1,50 +1,190 @@
+/**
+ * @file ControlsWidget.hpp
+ * @brief This file contains declaration of ControlsWidget class
+ * which is responsible for handling user interactions with the controls
+ * @author Mifka Radim (xmifka00)
+ * @date April 2024
+ */
 #pragma once
 
-#include <QWidget>
 #include <QObject>
+#include <QWidget>
 #include <memory>
 #include "GUI/Widget/ManualRobotControlsWidget.hpp"
 #include "GUI/Widget/ObstacleParametersWidget.hpp"
 #include "GUI/Widget/RobotParametersWidget.hpp"
 #include "GUI/Widget/SpawnControlsWidget.hpp"
 #include "Model/IMap.hpp"
-#include <memory>
 
+/**
+ * @class ControlsWidget
+ * @brief This class is responsible for handling user interactions with the
+ * controls in the GUI
+ */
 class ControlsWidget : public QWidget {
   Q_OBJECT
   std::shared_ptr<IMap> map;
   SpawnControlsWidget* spawnControls;
-  ObstacleParametersWidget* obstacleParameters;
   RobotParametersWidget* robotParameters;
   ManualRobotControlsWidget* manualRobotControls;
-  
+  ObstacleParametersWidget* obstacleParameters;
 
  public:
+  /**
+   * @brief Construct a new ControlsWidget object
+   *
+   * @param parent
+   */
   explicit ControlsWidget(QWidget* parent = nullptr);
 
+  /**
+   * @brief Set the Map object
+   *
+   * @param IMap map
+   */
   void setMap(std::shared_ptr<IMap> map) { this->map = map; }
 
-  void onObstacleSelected(std::shared_ptr<Obstacle> obstacle);
-  void onObstacleDeselected();
+  /**
+   * @brief Method called when robot is selected
+   *
+   * Shows robot parameters and hide spawn controls
+   *
+   * @param robot
+   */
   void onRobotSelected(std::shared_ptr<Robot> robot);
-  void onRobotDeselected();
-  void onRemoveObstacle(std::shared_ptr<Obstacle> obstacle);
-  void onRemoveRobot(std::shared_ptr<Robot> robot);
 
- signals:
-  void loadMapClicked();
-  void saveMapClicked();
-  void startClicked();
-  void stopClicked();
-  void addObstacleClicked();
-  void addRobotClicked();
-  void updateMap();
+  /**
+   * @brief Method called when robot is deselected
+   *
+   * Shows spawn controls and hides robot parameters
+   *
+   */
+  void onRobotDeselected();
+
+  /**
+   * @brief Method called when robot should be removed
+   *
+   * Removes robot from map
+   * Hides robot parameters and shows spawn controls
+   *
+   * @param robot
+   */
+  void removeRobot(std::shared_ptr<Robot> robot);
+
+  /**
+   * @brief Method called when obstacle is selected
+   *
+   * Shows obstacle parameters and hide spawn controls
+   *
+   * @param obstacle
+   */
+  void onObstacleSelected(std::shared_ptr<Obstacle> obstacle);
+
+  /**
+   * @brief Method called when obstacle is deselected
+   *
+   * Hides obstacle parameters and shows spawn controls
+   *
+   */
+  void onObstacleDeselected();
+
+  /**
+   * @brief Method called when obstacle should be removed
+   *
+   * Removes obstacle from map
+   * Hides obstacle parameters and shows spawn controls
+   *
+   * @param obstacle
+   */
+  void removeObstacle(std::shared_ptr<Obstacle> obstacle);
 
  private:
+  /**
+   * @brief Method called when load map button is clicked
+   *
+   * emits loadMapClicked signal
+   *
+   */
   void onLoadMapClicked();
+
+  /**
+   * @brief Method called when save map button is clicked
+   *
+   * emits saveMapClicked signal
+   *
+   */
   void onSaveMapClicked();
+
+  /**
+   * @brief Method called when start button is clicked
+   *
+   * emits startClicked signal
+   */
   void onStartClicked();
+
+  /**
+   * @brief Method called when stop button is clicked
+   *
+   */
   void onStopClicked();
+
+  /**
+   * @brief Method called when add obstacle button is clicked
+   *
+   * emits addObstacleClicked signal
+   *
+   */
   void onAddObstacle();
+
+  /**
+   * @brief Method called when add robot button is clicked
+   *
+   * Adds robot to map based on type
+   *
+   * @param Robot::Type type
+   */
   void onAddRobot(Robot::Type type);
+
+ signals:
+  /**
+   * @brief Signal emitted when map should be loaded
+   *
+   */
+  void loadMapClicked();
+
+  /**
+   * @brief Signal emitted when map should be saved
+   *
+   */
+  void saveMapClicked();
+
+  /**
+   * @brief Signal emitted when start button is clicked
+   *
+   */
+  void startClicked();
+
+  /**
+   * @brief Signal emitted when stop button is clicked
+   *
+   */
+  void stopClicked();
+
+  /**
+   * @brief Signal emitted when add obstacle button is clicked
+   *
+   */
+  void addObstacleClicked();
+
+  /**
+   * @brief Signal emitted when add robot button is clicked
+   *
+   */
+  void addRobotClicked();
+
+  /**
+   * @brief Signal emitted when map needs to be updated
+   *
+   */
+  void updateMap();
 };
--- src/GUI/Widget/ControlsWidget.cpp
+++ src/GUI/Widget/ControlsWidget.cpp
@@ -1,82 +1,97 @@
+/**
+ * @file ControlsWidget.cpp
+ * @brief This file contains declaration of ControlsWidget class
+ * which is responsible for handling user interactions with the controls
+ * @author Mifka Radim (xmifka00)
+ * @date April 2024
+ */
 #include "GUI/Widget/ControlsWidget.hpp"
+#include <QVBoxLayout>
 #include "GUI/Widget/SimulationControlsWidget.hpp"
 #include "GUI/Widget/SpawnControlsWidget.hpp"
 #include "Model/Map.hpp"
 #include "Settings.hpp"
-#include <QVBoxLayout>
 
 ControlsWidget::ControlsWidget(QWidget* parent) : QWidget(parent) {
   // load empty map
-map = std::make_shared<Map>();
+  map = std::make_shared<Map>();
 
-QVBoxLayout* layout = new QVBoxLayout(this);
+  QVBoxLayout* layout = new QVBoxLayout(this);
 
-SimulationControlsWidget* simulationControls = new SimulationControlsWidget(this);
-layout->addWidget(simulationControls);
+  SimulationControlsWidget* simulationControls =
+      new SimulationControlsWidget(this);
+  layout->addWidget(simulationControls);
 
-connect(simulationControls, &SimulationControlsWidget::loadMapClicked, this, &ControlsWidget::onLoadMapClicked);
-connect(simulationControls, &SimulationControlsWidget::saveMapClicked, this, &ControlsWidget::onSaveMapClicked);
-connect(simulationControls, &SimulationControlsWidget::startClicked, this, &ControlsWidget::onStartClicked);
-connect(simulationControls, &SimulationControlsWidget::stopClicked, this, &ControlsWidget::onStopClicked);
+  connect(simulationControls, &SimulationControlsWidget::loadMapClicked, this,
+          &ControlsWidget::onLoadMapClicked);
+  connect(simulationControls, &SimulationControlsWidget::saveMapClicked, this,
+          &ControlsWidget::onSaveMapClicked);
+  connect(simulationControls, &SimulationControlsWidget::startClicked, this,
+          &ControlsWidget::onStartClicked);
+  connect(simulationControls, &SimulationControlsWidget::stopClicked, this,
+          &ControlsWidget::onStopClicked);
 
-spawnControls = new SpawnControlsWidget(this);
-layout->addWidget(spawnControls);
+  spawnControls = new SpawnControlsWidget(this);
+  layout->addWidget(spawnControls);
 
-connect(spawnControls, &SpawnControlsWidget::addObstacleClicked, this, &ControlsWidget::onAddObstacle);
-connect(spawnControls, &SpawnControlsWidget::addManualRobotClicked, this, [this]() {
-    this->onAddRobot(Robot::Robot::Type::MANUAL);
-});
-connect(spawnControls, &SpawnControlsWidget::addAutoRobotClicked, this, [this]() {
-    this->onAddRobot(Robot::Robot::Type::AUTONOMOUS);
-});
+  connect(spawnControls, &SpawnControlsWidget::addObstacleClicked, this,
+          &ControlsWidget::onAddObstacle);
+  connect(spawnControls, &SpawnControlsWidget::addManualRobotClicked, this,
+          [this]() { this->onAddRobot(Robot::Robot::Type::MANUAL); });
+  connect(spawnControls, &SpawnControlsWidget::addAutoRobotClicked, this,
+          [this]() { this->onAddRobot(Robot::Robot::Type::AUTONOMOUS); });
 
-obstacleParameters = new ObstacleParametersWidget(this);
-layout->addWidget(obstacleParameters);
-obstacleParameters->hide();
+  obstacleParameters = new ObstacleParametersWidget(this);
+  layout->addWidget(obstacleParameters);
+  obstacleParameters->hide();
 
-connect(obstacleParameters, &ObstacleParametersWidget::updateMap, this, &ControlsWidget::updateMap);
-connect(obstacleParameters, &ObstacleParametersWidget::removeObstacle, this, &ControlsWidget::onRemoveObstacle);
+  connect(obstacleParameters, &ObstacleParametersWidget::updateMap, this,
+          &ControlsWidget::updateMap);
+  connect(obstacleParameters, &ObstacleParametersWidget::removeObstacle, this,
+          &ControlsWidget::removeObstacle);
 
-robotParameters = new RobotParametersWidget(this);
-layout->addWidget(robotParameters);
-robotParameters->hide();
+  robotParameters = new RobotParametersWidget(this);
+  layout->addWidget(robotParameters);
+  robotParameters->hide();
 
-connect(robotParameters, &RobotParametersWidget::updateMap, this, &ControlsWidget::updateMap);
-connect(robotParameters, &RobotParametersWidget::removeRobot, this, &ControlsWidget::onRemoveRobot);
+  connect(robotParameters, &RobotParametersWidget::updateMap, this,
+          &ControlsWidget::updateMap);
+  connect(robotParameters, &RobotParametersWidget::removeRobot, this,
+          &ControlsWidget::removeRobot);
 
-manualRobotControls = new ManualRobotControlsWidget(this);
-layout->addWidget(manualRobotControls);
-manualRobotControls->hide();
+  manualRobotControls = new ManualRobotControlsWidget(this);
+  layout->addWidget(manualRobotControls);
+  manualRobotControls->hide();
 
-layout->addStretch();
-setLayout(layout);
+  layout->addStretch();
+  setLayout(layout);
 }
 
 void ControlsWidget::onObstacleSelected(std::shared_ptr<Obstacle> obstacle) {
-    obstacleParameters->setObstacle(obstacle);
-    obstacleParameters->show();
-    spawnControls->hide();
+  obstacleParameters->setObstacle(obstacle);
+  obstacleParameters->show();
+  spawnControls->hide();
 }
 
 void ControlsWidget::onObstacleDeselected() {
-    this->obstacleParameters->hide();
-    spawnControls->show();
+  this->obstacleParameters->hide();
+  spawnControls->show();
 }
 
 void ControlsWidget::onRobotSelected(std::shared_ptr<Robot> robot) {
-    robotParameters->setRobot(robot);
-    robotParameters->show();
-    spawnControls->hide();
-    if (robot->getType() == Robot::Type::MANUAL) {
-        manualRobotControls->setRobot(std::static_pointer_cast<ManualRobot>(robot));
-        manualRobotControls->show();
-    };
+  robotParameters->setRobot(robot);
+  robotParameters->show();
+  spawnControls->hide();
+  if (robot->getType() == Robot::Type::MANUAL) {
+    manualRobotControls->setRobot(std::static_pointer_cast<ManualRobot>(robot));
+    manualRobotControls->show();
+  };
 }
 
 void ControlsWidget::onRobotDeselected() {
-    robotParameters->hide();
-    manualRobotControls->hide();
-    spawnControls->show();
+  robotParameters->hide();
+  manualRobotControls->hide();
+  spawnControls->show();
 }
 
 void ControlsWidget::onLoadMapClicked() {
@@ -97,24 +112,30 @@ void ControlsWidget::onStopClicked() {
 
 void ControlsWidget::onAddObstacle() {
   emit stopClicked();
-  map->addObstacle(map->getWidth() / 2, map->getHeight() / 2, Settings::OBSTACLE_BASE_WIDTH, Settings::OBSTACLE_BASE_HEIGHT, Settings::OBSTACLE_BASE_ROTATION);
+  map->addObstacle(
+      map->getWidth() / 2, map->getHeight() / 2, Settings::OBSTACLE_BASE_WIDTH,
+      Settings::OBSTACLE_BASE_HEIGHT, Settings::OBSTACLE_BASE_ROTATION);
   emit updateMap();
 }
 
 void ControlsWidget::onAddRobot(Robot::Type type) {
   emit stopClicked();
-  map->addRobot(map->getWidth() / 2, map->getHeight() / 2, Settings::ROBOT_BASE_DIAMETER, Settings::ROBOT_BASE_VIEW_ANGLE, Settings::ROBOT_BASE_ROTATE_ANGLE, Settings::ROBOT_BASE_COLISION_DISTANCE, Settings::ROBOT_BASE_ROTATE_CLOCKWISE, Settings::ROBOT_BASE_SPEED, type);
+  map->addRobot(
+      map->getWidth() / 2, map->getHeight() / 2, Settings::ROBOT_BASE_DIAMETER,
+      Settings::ROBOT_BASE_VIEW_ANGLE, Settings::ROBOT_BASE_ROTATE_ANGLE,
+      Settings::ROBOT_BASE_COLISION_DISTANCE,
+      Settings::ROBOT_BASE_ROTATE_CLOCKWISE, Settings::ROBOT_BASE_SPEED, type);
   emit updateMap();
 }
 
-void ControlsWidget::onRemoveObstacle(std::shared_ptr<Obstacle> obstacle) {
+void ControlsWidget::removeObstacle(std::shared_ptr<Obstacle> obstacle) {
   map->removeObstacle(obstacle);
   this->obstacleParameters->hide();
   this->spawnControls->show();
   emit updateMap();
 }
 
-void ControlsWidget::onRemoveRobot(std::shared_ptr<Robot> robot) {
+void ControlsWidget::removeRobot(std::shared_ptr<Robot> robot) {
   map->removeRobot(robot);
   this->robotParameters->hide();
   this->manualRobotControls->hide();
