d7fa5380ccc49da71c33da09574015d63b5dd056
Radim Mifka <radimmifka@gmail.com>
2024-05-04 01:19:07 +0200

    docs: ManualRobotControlsWidget doxygen

--- include/GUI/Widget/ManualRobotControlsWidget.hpp
+++ include/GUI/Widget/ManualRobotControlsWidget.hpp
@@ -1,18 +1,59 @@
+/**
+ * @file ManulRobotControlsWidget.hpp
+ * @brief This file contains declaration of ManualRobotControlsWidget class
+ * which is responsible for handling user interactions with the manual robot
+ * controls
+ * @author Mifka Radim (xmifka00)
+ * @date April 2024
+ */
 #pragma once
 #include <QWidget>
 #include "Model/Robot/ManualRobot.hpp"
 
-class ManualRobotControlsWidget: public QWidget {
+/**
+ * @class ManualRobotControlsWidget
+ * @brief This class is responsible for handling user interactions with the
+ * manual robot controls in the GUI
+ */
+class ManualRobotControlsWidget : public QWidget {
   Q_OBJECT
-    std::shared_ptr<ManualRobot> robot;
+  std::shared_ptr<ManualRobot> robot;
 
-    public:
-        ManualRobotControlsWidget(QWidget* parent = nullptr);
-        void setRobot(std::shared_ptr<ManualRobot> robot);
+ public:
+  /**
+   * @brief Construct a new ManualRobotControlsWidget object
+   * @param parent
+   */
+  ManualRobotControlsWidget(QWidget* parent = nullptr);
 
-    private:
-        void onMoveClicked();
-        void onStopClicked();
-        void onTurnLeftClicked();
-        void onTurnRightClicked();
+  /**
+   * @brief Set the Robot object
+   * @param std::shared_ptr<ManualRobot> robot
+   */
+  void setRobot(std::shared_ptr<ManualRobot> robot);
+
+ private:
+  /**
+   * @brief Method called when move button is clicked
+   * It sets the robot state to move
+   */
+  void onMoveClicked();
+
+  /**
+   * @brief Method called when stop button is clicked
+   * It sets the robot state to stop
+   */
+  void onStopClicked();
+
+  /**
+   * @brief Method called when turn left button is clicked
+   * It sets the robot state to turn left
+   */
+  void onTurnLeftClicked();
+
+  /**
+   * @brief Method called when turn right button is clicked
+   * It sets the robot state to turn right
+   */
+  void onTurnRightClicked();
 };
--- src/GUI/Widget/ManualRobotControlsWidget.cpp
+++ src/GUI/Widget/ManualRobotControlsWidget.cpp
@@ -1,78 +1,96 @@
+/**
+ * @file ManulRobotControlsWidget.cpp
+ * @brief This file contains declaration of ManualRobotControlsWidget class
+ * which is responsible for handling user interactions with the manual robot
+ * controls
+ * @author Mifka Radim (xmifka00)
+ * @date April 2024
+ */
 #include "GUI/Widget/ManualRobotControlsWidget.hpp"
+#include <QShortcut>
+#include <QVBoxLayout>
 #include "GUI/Button.hpp"
 #include "GUI/Frame.hpp"
 #include "GUI/Label.hpp"
-#include <QVBoxLayout>
-#include <QShortcut>
 
-ManualRobotControlsWidget::ManualRobotControlsWidget(QWidget* parent) : QWidget(parent) {
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    Frame *frame = new Frame(this);
-    frame->setObjectName("frame");
-    QVBoxLayout *frameLayout = new QVBoxLayout(frame);
+ManualRobotControlsWidget::ManualRobotControlsWidget(QWidget* parent)
+    : QWidget(parent) {
+  QVBoxLayout* layout = new QVBoxLayout(this);
+  Frame* frame = new Frame(this);
+  frame->setObjectName("frame");
+  QVBoxLayout* frameLayout = new QVBoxLayout(frame);
 
-    Label *label = new Label("Manual controls");
-    frameLayout->addWidget(label, 0, Qt::AlignCenter);
+  Label* label = new Label("Manual controls");
+  frameLayout->addWidget(label, 0, Qt::AlignCenter);
 
-    auto bottom = new QHBoxLayout();
-    auto moveButton = new Button("Move");
-    auto stopButton = new Button("Stop");
-    auto turnLeftButton = new Button("Turn Left");
-    auto turnRightButton = new Button("Turn Right");
+  auto bottom = new QHBoxLayout();
+  auto moveButton = new Button("Move");
+  auto stopButton = new Button("Stop");
+  auto turnLeftButton = new Button("Turn Left");
+  auto turnRightButton = new Button("Turn Right");
 
-    frameLayout->addWidget(moveButton);
+  frameLayout->addWidget(moveButton);
 
-    bottom->addWidget(turnLeftButton);
-    bottom->addWidget(stopButton);
-    bottom->addWidget(turnRightButton);
+  bottom->addWidget(turnLeftButton);
+  bottom->addWidget(stopButton);
+  bottom->addWidget(turnRightButton);
 
+  frameLayout->addLayout(bottom);
 
-    frameLayout->addLayout(bottom);
+  layout->addWidget(frame);
 
-    layout->addWidget(frame);
+  QShortcut* moveShortcut = new QShortcut(QKeySequence(Qt::Key_W), moveButton);
+  connect(moveShortcut, &QShortcut::activated, this,
+          &ManualRobotControlsWidget::onMoveClicked);
 
-    QShortcut *moveShortcut = new QShortcut(QKeySequence(Qt::Key_W), moveButton);
-    connect(moveShortcut, &QShortcut::activated, this, &ManualRobotControlsWidget::onMoveClicked);
+  QShortcut* turnLeftShortcut =
+      new QShortcut(QKeySequence(Qt::Key_A), turnLeftButton);
+  connect(turnLeftShortcut, &QShortcut::activated, this,
+          &ManualRobotControlsWidget::onTurnLeftClicked);
 
-    QShortcut *turnLeftShortcut = new QShortcut(QKeySequence(Qt::Key_A), turnLeftButton);
-    connect(turnLeftShortcut, &QShortcut::activated, this, &ManualRobotControlsWidget::onTurnLeftClicked);
+  QShortcut* turnRightShortcut =
+      new QShortcut(QKeySequence(Qt::Key_D), turnRightButton);
+  connect(turnRightShortcut, &QShortcut::activated, this,
+          &ManualRobotControlsWidget::onTurnRightClicked);
 
-    QShortcut *turnRightShortcut = new QShortcut(QKeySequence(Qt::Key_D), turnRightButton);
-    connect(turnRightShortcut, &QShortcut::activated, this, &ManualRobotControlsWidget::onTurnRightClicked);
+  QShortcut* stopShortcut = new QShortcut(QKeySequence(Qt::Key_S), stopButton);
+  connect(stopShortcut, &QShortcut::activated, this,
+          &ManualRobotControlsWidget::onStopClicked);
 
-    QShortcut *stopShortcut = new QShortcut(QKeySequence(Qt::Key_S), stopButton);
-    connect(stopShortcut, &QShortcut::activated, this, &ManualRobotControlsWidget::onStopClicked);
-
-    connect(moveButton, &Button::clicked, this, &ManualRobotControlsWidget::onMoveClicked);
-    connect(turnLeftButton, &Button::clicked, this, &ManualRobotControlsWidget::onTurnLeftClicked);
-    connect(stopButton, &Button::clicked, this, &ManualRobotControlsWidget::onStopClicked);
-    connect(turnRightButton, &Button::clicked, this, &ManualRobotControlsWidget::onTurnRightClicked);
+  connect(moveButton, &Button::clicked, this,
+          &ManualRobotControlsWidget::onMoveClicked);
+  connect(turnLeftButton, &Button::clicked, this,
+          &ManualRobotControlsWidget::onTurnLeftClicked);
+  connect(stopButton, &Button::clicked, this,
+          &ManualRobotControlsWidget::onStopClicked);
+  connect(turnRightButton, &Button::clicked, this,
+          &ManualRobotControlsWidget::onTurnRightClicked);
 }
 
 void ManualRobotControlsWidget::setRobot(std::shared_ptr<ManualRobot> robot) {
-    this->robot = robot;
+  this->robot = robot;
 }
 
 void ManualRobotControlsWidget::onMoveClicked() {
-    if (robot) {
-        robot->setState(ManualRobot::State::MOVE);
-    }
+  if (robot) {
+    robot->setState(ManualRobot::State::MOVE);
+  }
 }
 
 void ManualRobotControlsWidget::onStopClicked() {
-    if (robot) {
-        robot->setState(ManualRobot::State::STOP);
-    }
+  if (robot) {
+    robot->setState(ManualRobot::State::STOP);
+  }
 }
 
 void ManualRobotControlsWidget::onTurnLeftClicked() {
-    if (robot) {
-        robot->setState(ManualRobot::State::TURN_LEFT);
-    }
+  if (robot) {
+    robot->setState(ManualRobot::State::TURN_LEFT);
+  }
 }
 
 void ManualRobotControlsWidget::onTurnRightClicked() {
-    if (robot) {
-        robot->setState(ManualRobot::State::TURN_RIGHT);
-    }
+  if (robot) {
+    robot->setState(ManualRobot::State::TURN_RIGHT);
+  }
 }
